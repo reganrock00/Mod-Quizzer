0b82584e6e08502ca34e9521baddec7c8f78f5c9
Regan.Archibald <Regan.Archibald@MU-C1-08.mullen.pvt.k12.co.us>
2016-05-10 08:59:33 -0600

    jframe update

--- Modulus Quizzer/src/ModQuizzer.java
+++ Modulus Quizzer/src/ModQuizzer.java
@@ -1,49 +1,51 @@
 import java.util.Scanner;
 
-public class ModQuizzer
-	{
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
+import java.awt.*;
+
+public class ModQuizzer extends JFrame
+	{
+        static int choice;
+        static JFrame frame = new JFrame();
 		public static void main(String[] args)
 			{
 			Scanner userInput =new Scanner (System.in);
-			System.out.println("How many questions do you want?");
-			int choice = userInput.nextInt();
+			int choice = Integer.parseInt(JOptionPane.showInputDialog("How many questions do you want?"));
 			String [] questions = new String [choice];
 			int countRight = 0;
 			int countWrong = 0;
 			for (int i = 0; i < questions.length; i++) 
 				{
-				Scanner userInput2 =new Scanner (System.in);
 				if (i == 3)
 					{
 					int randomNumber4 = (int)(Math.random()*25)+1;
 					int randomNumber5 = (int)(Math.random()*50)+25;
-					System.out.println("What is " + (randomNumber4) + " % " + (randomNumber5) + "=");
-					int guess1 = userInput2.nextInt();
+					int guess1 = Integer.parseInt(JOptionPane.showInputDialog("What is " + (randomNumber4) + " % " + (randomNumber5) + "="));
 					if (randomNumber4 % randomNumber5 == guess1)
 					   {
-						System.out.println("Correct!");
+						JOptionPane.showMessageDialog(frame, "Correct");
 						countRight++;
 					   }
 				    else if (randomNumber4 % randomNumber5 != guess1)
 					   {
-						System.out.println("Incorrect. The correct answer was " + (randomNumber4 % randomNumber5));
+						JOptionPane.showMessageDialog(frame, "Incorrect. The correct answer was " + (randomNumber4 % randomNumber5));
 						countWrong++;
 					   }
 					}
 				else if (i == 2)
 					{
 					int randomNumber3 = (int)(Math.random()*25)+1;
-					System.out.println("What is " + (randomNumber3) + " % " + (randomNumber3) + "=");
-					int guess2 = userInput2.nextInt();
+					int guess2 = Integer.parseInt(JOptionPane.showInputDialog("What is " + (randomNumber3) + " % " + (randomNumber3) + "="));
 					if (randomNumber3 % randomNumber3 == guess2)
 					   {
-						System.out.println("Correct!");
+						JOptionPane.showMessageDialog(frame, "Correct");
 						countRight++;
 					   }
 				    else if (randomNumber3 % randomNumber3 != guess2)
 					   {
-						System.out.println("Incorrect. The correct answer was " + (randomNumber3 % randomNumber3));
+						JOptionPane.showMessageDialog(frame, "Incorrect. The correct answer was " + (randomNumber3 % randomNumber3));
 						countWrong++;
 						}
 					 }
@@ -51,21 +53,20 @@ public class ModQuizzer
 					{
 					int randomNumber1 = (int)(Math.random()*50)+25;
 				    int randomNumber2 = (int)(Math.random()*25)+1;
-				    System.out.println("What is " + (randomNumber1) + " % " + (randomNumber2) + "=");
-				    int guess3 = userInput2.nextInt();
+				    int guess3 = Integer.parseInt(JOptionPane.showInputDialog("What is " + (randomNumber1) + " % " + (randomNumber2) + "="));
 				    if (randomNumber1 % randomNumber2 == guess3)
 					   {
-						System.out.println("Correct!");
+						JOptionPane.showMessageDialog(frame, "Correct");
 						countRight++;
 					   }
 				    else if (randomNumber1 % randomNumber2 != guess3)
 					   {
-						System.out.println("Incorrect. The correct answer was " + (randomNumber1 % randomNumber2));
+						JOptionPane.showMessageDialog(frame, "Incorrect. The correct answer was " + (randomNumber1 % randomNumber2));
 						countWrong++;
 					   }
 					}
 				}
-			System.out.println("You got " + countRight + " questions right and " + countWrong + " wrong!");
+			JOptionPane.showMessageDialog(frame, "You got " + countRight + " questions right and " + countWrong + " wrong!");
 			}
 
 	}
